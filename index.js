417e4d03239dcf85c7e66da5f6cfcf259e6b511d
John Otander <johnotander@gmail.com>
2016-10-01 12:00:29 -0600

    Include response time

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const got = require('got')
 
-module.exports = url => got.head(url)
+module.exports = url => {
+  const start = process.hrtime()
+
+  return got.head(url).then(res => receive(res, start))
+}
+
+const receive = (res, start) => {
+  const diff = process.hrtime(start)
+  const total = diff[0] * 1e3 + diff[1] * 1e-6
+
+  return {
+    code: res.statusCode,
+    time: total,
+    headers: res.headers
+  }
+}
+
+// Response time algorithm from Doug Wilson in response-time
+//   => https://github.com/expressjs/response-time/blob/1e8b4be1378ec4bc6371f0f8c16441e17fd09c5a/index.js#L53
--- test.js
+++ test.js
@@ -1,7 +1,13 @@
 import test from 'ava'
+import isPresent from 'is-present'
+
 import heady from './'
 
-test('heady does something awesome', async t => {
-  const res = await heady('google.com')
-  t.is(res.statusCode, 200)
+test('returns successfully when it should', async t => {
+  t.plan(2)
+
+  const res = await heady('https://google.com')
+
+  t.is(res.code, 200)
+  t.ok(isPresent(res.headers))
 })
